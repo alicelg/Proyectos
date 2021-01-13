1044f806363a0e6434dc44593ed40a2439141436
alicelg <alicelop@ucm.es>
2021-01-13 18:08:49 +0100

    creación de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DatosComponent } from './datos/datos.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
 import { HomeComponent } from './home/home.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { TecnologiasComponent } from './tecnologias/tecnologias.component';
 
 
 const routes: Routes = [
-  {
-    path: 'home', component: HomeComponent
-  }
+  { path: 'home', component: HomeComponent },
+  { path: 'proyectos', component: ProyectosComponent },
+  { path: 'tecnologias', component: TecnologiasComponent },
+  { path: 'experiencia', component: ExperienciaComponent },
+  { path: 'datos', component: DatosComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { DatosComponent } from './datos/datos.component';
+import { TecnologiasComponent } from './tecnologias/tecnologias.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ProyectosComponent,
+    ExperienciaComponent,
+    DatosComponent,
+    TecnologiasComponent
   ],
   imports: [
     BrowserModule,
